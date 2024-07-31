cc8537ee512f8348f94c6d0ab6316060a4f9af04
Alimkhan <magomadovv@mail.ru>
2024-07-31 21:09:20 +0300

    Last commit

--- index.js
+++ index.js
@@ -19,18 +19,18 @@ console.log(DaysOfWeek.Sunday); // 6
 // Создайте перечисление Months, содержащее названия месяцев (январь, февраль и т.д.). Напишите функцию, которая принимает в качестве аргумента число от 1 до 12 и возвращает название соответствующего месяца в консоль.
 var Months;
 (function (Months) {
-    Months[Months["January"] = 0] = "January";
-    Months[Months["February"] = 1] = "February";
-    Months[Months["March"] = 2] = "March";
-    Months[Months["April"] = 3] = "April";
-    Months[Months["May"] = 4] = "May";
-    Months[Months["June"] = 5] = "June";
-    Months[Months["July"] = 6] = "July";
-    Months[Months["August"] = 7] = "August";
-    Months[Months["September"] = 8] = "September";
-    Months[Months["October"] = 9] = "October";
-    Months[Months["November"] = 10] = "November";
-    Months[Months["December"] = 11] = "December";
+    Months[Months["January"] = 1] = "January";
+    Months[Months["February"] = 2] = "February";
+    Months[Months["March"] = 3] = "March";
+    Months[Months["April"] = 4] = "April";
+    Months[Months["May"] = 5] = "May";
+    Months[Months["June"] = 6] = "June";
+    Months[Months["July"] = 7] = "July";
+    Months[Months["August"] = 8] = "August";
+    Months[Months["September"] = 9] = "September";
+    Months[Months["October"] = 10] = "October";
+    Months[Months["November"] = 11] = "November";
+    Months[Months["December"] = 12] = "December";
 })(Months || (Months = {}));
 function numberMonths(monthNumber) {
     switch (monthNumber) {
@@ -73,3 +73,71 @@ function numberMonths(monthNumber) {
 }
 numberMonths(7);
 numberMonths(10);
+// Создайте перечисление Colors, содержащее несколько цветов (красный, синий, зеленый и т.д.). Напишите функцию, которая принимает в качестве аргумента цвет и выводит в консоль сообщение вида "Выбран [цвет] цвет".
+var Colors;
+(function (Colors) {
+    Colors[Colors["Red"] = 1] = "Red";
+    Colors[Colors["blue"] = 2] = "blue";
+    Colors[Colors["green"] = 3] = "green";
+    Colors[Colors["black"] = 4] = "black";
+    Colors[Colors["white"] = 5] = "white";
+})(Colors || (Colors = {}));
+function colorVib(color) {
+    switch (color) {
+        case Colors.Red:
+            console.log("Выбран красный цвет");
+            break;
+        case Colors.blue:
+            console.log("Выбран синий цвет");
+            break;
+        case Colors.green:
+            console.log("Выбран зеленый цвет");
+            break;
+        case Colors.black:
+            console.log("Выбран черный цвет");
+            break;
+        case Colors.white:
+            console.log("Выбран белый цвет");
+    }
+}
+colorVib(Colors.black);
+colorVib(Colors.white);
+// Создайте перечисление Planets, содержащее названия планет (Меркурий, Венера, Земля и т.д.). Напишите функцию, которая принимает в качестве аргумента название планеты и возвращает порядковый номер этой планеты в солнечной системе.
+var Planets;
+(function (Planets) {
+    Planets[Planets["Mercury"] = 1] = "Mercury";
+    Planets[Planets["Venus"] = 2] = "Venus";
+    Planets[Planets["Earth"] = 3] = "Earth";
+    Planets[Planets["Mars"] = 4] = "Mars";
+    Planets[Planets["Jupiter"] = 5] = "Jupiter";
+    Planets[Planets["Saturn"] = 6] = "Saturn";
+    Planets[Planets["Uranus"] = 7] = "Uranus";
+    Planets[Planets["Neptune"] = 8] = "Neptune";
+})(Planets || (Planets = {}));
+function planetColv(planet) {
+    return planet;
+}
+console.log(planetColv(Planets.Jupiter));
+console.log(planetColv(Planets.Uranus));
+// Создайте перечисление Fruits, содержащее названия фруктов (яблоко, груша, апельсин и т.д.). Напишите функцию, которая принимает в качестве аргумента фрукт и проверяет, входит ли он в перечисление Fruits. Если фрукт есть в перечислении, выведите в консоль сообщение "Этот фрукт вкусный!". Если фрукта нет в перечислении, выведите сообщение "Неизвестный фрукт!".
+var Fruits;
+(function (Fruits) {
+    Fruits[Fruits["Apple"] = 0] = "Apple";
+    Fruits[Fruits["Pear"] = 1] = "Pear";
+    Fruits[Fruits["Orange"] = 2] = "Orange";
+    Fruits[Fruits["Banana"] = 3] = "Banana";
+    Fruits[Fruits["Strawberry"] = 4] = "Strawberry";
+    Fruits[Fruits["Grape"] = 5] = "Grape";
+})(Fruits || (Fruits = {}));
+function checkFruit(fruit) {
+    if (Object.values(Fruits).includes(fruit)) {
+        console.log("Этот фрукт вкусный!");
+    }
+    else {
+        console.log("Неизвестный фрукт!");
+    }
+}
+checkFruit("Apple");
+checkFruit("Grape");
+checkFruit("Pineapple");
+// Я не догадался использовать "If,else", и не смог решить.Так что списал у GPT
--- index.ts
+++ index.ts
@@ -21,18 +21,18 @@ console.log(DaysOfWeek.Sunday); // 6
 // Создайте перечисление Months, содержащее названия месяцев (январь, февраль и т.д.). Напишите функцию, которая принимает в качестве аргумента число от 1 до 12 и возвращает название соответствующего месяца в консоль.
 
 enum Months {
-  January,
-  February,
-  March,
-  April,
-  May,
-  June,
-  July,
-  August,
-  September,
-  October,
-  November,
-  December
+  January = 1,
+  February = 2,
+  March = 3,
+  April = 4,
+  May = 5,
+  June = 6,
+  July = 7,
+  August = 8,
+  September = 9,
+  October = 10,
+  November = 11,
+  December = 12
 }
 
 function numberMonths(monthNumber: number): void {
@@ -78,3 +78,78 @@ function numberMonths(monthNumber: number): void {
 numberMonths(7);
 numberMonths(10);
 
+// Создайте перечисление Colors, содержащее несколько цветов (красный, синий, зеленый и т.д.). Напишите функцию, которая принимает в качестве аргумента цвет и выводит в консоль сообщение вида "Выбран [цвет] цвет".
+
+enum Colors {
+  Red = 1,
+  blue = 2,
+  green = 3,
+  black = 4,
+  white = 5
+}
+
+function colorVib(color: Colors): void {
+  switch(color) {
+    case Colors.Red:
+      console.log("Выбран красный цвет");
+      break;
+    case Colors.blue:
+      console.log("Выбран синий цвет");
+      break;
+    case Colors.green:
+      console.log("Выбран зеленый цвет");
+      break;
+    case Colors.black:
+      console.log("Выбран черный цвет");
+      break;
+    case Colors.white:
+      console.log("Выбран белый цвет");
+  }
+}
+
+colorVib(Colors.black)
+colorVib(Colors.white)
+
+// Создайте перечисление Planets, содержащее названия планет (Меркурий, Венера, Земля и т.д.). Напишите функцию, которая принимает в качестве аргумента название планеты и возвращает порядковый номер этой планеты в солнечной системе.
+
+enum Planets {
+  Mercury = 1,
+  Venus = 2,
+  Earth = 3,
+  Mars = 4,
+  Jupiter = 5,
+  Saturn = 6,
+  Uranus = 7,
+  Neptune = 8
+}
+
+function planetColv(planet: Planets): number {
+  return planet;
+}
+
+console.log(planetColv(Planets.Jupiter)); 
+console.log(planetColv(Planets.Uranus)); 
+
+// Создайте перечисление Fruits, содержащее названия фруктов (яблоко, груша, апельсин и т.д.). Напишите функцию, которая принимает в качестве аргумента фрукт и проверяет, входит ли он в перечисление Fruits. Если фрукт есть в перечислении, выведите в консоль сообщение "Этот фрукт вкусный!". Если фрукта нет в перечислении, выведите сообщение "Неизвестный фрукт!".
+
+enum Fruits {
+  Apple,
+  Pear,
+  Orange,
+  Banana,
+  Strawberry,
+  Grape
+}
+
+function checkFruit(fruit: string): void {
+  if (Object.values(Fruits).includes(fruit as Fruits)) {
+    console.log("Этот фрукт вкусный!");
+  } else {
+    console.log("Неизвестный фрукт!");
+  }
+}
+
+checkFruit("Apple");
+checkFruit("Grape"); 
+checkFruit("Pineapple"); 
+// Я не догадался использовать "If,else", и не смог решить.Так что списал у GPT
